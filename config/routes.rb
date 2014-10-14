fdd6216aea1a6259e7406b5d7c99183f1c395139
Margo Urey <margo@margonline.co.uk>
2014-10-14 16:19:46 +0100

    crud for recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  resources :recipes, :only => [:index, :show]
+  resources :recipes, :only => [:index, :show, :create, :update, :destroy]
 
 end
---
+++ spec/features/view_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper.rb'
+
+feature "viewing a recipe", js: true do
+  before do
+    Recipe.create!(name: 'Baked Potato w/ Cheese',
+            instructions: 'nuke for 20 minutes')
+
+    Recipe.create!(name: 'Baked Brussel Sprouts',
+            instructions: 'Slather in oil and roast on high heat for 40 minutes')
+  end
+  scenario "view one recipe" do
+    visit '/'
+    fill_in "keywords", with: "baked"
+    click_on "Search"
+
+    click_on "Baked Brussel Sprouts"
+
+    expect(page).to have_content("Baked Brussel Sprouts")
+    expect(page).to have_content("Slather in oil")
+
+    click_on "Back"
+
+    expect(page).to have_content("Baked Potato w/ Cheese")
+    expect(page).to_not have_content("Slather in oil")
+  end
+
+end
